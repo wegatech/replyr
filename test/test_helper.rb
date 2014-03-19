9387708dcdade781a2a6a99965e8c547b1a50971
wursttheke <philipp@caution-media.de>
2014-03-19 17:51:06 +0100

    remove loading fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,8 +19,3 @@ def setup_database
   ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS comments")
   ActiveRecord::Base.connection.execute("CREATE TABLE comments (id INTEGER PRIMARY KEY, body TEXT, user_id INTEGER)")
 end
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
