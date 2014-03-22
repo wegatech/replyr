44e2010032175e937cf50ac8b98dca4267de0595
wursttheke <philipp.wuellner@rwth-aachen.de>
2014-03-22 18:06:11 +0100

    remove bug in test

--- test/replyr/reply_email_test.rb
+++ test/replyr/reply_email_test.rb
@@ -11,7 +11,7 @@ describe Replyr::ReplyEmail do
   end
 
   it 'parses multipart message object correctly' do
-    mail = Mail.read('test/replyr/emails/reply_plain.eml')
+    mail = Mail.read('test/replyr/emails/reply_multipart.eml')
     reply_email = Replyr::ReplyEmail.new(mail)
     assert_equal "wursttheke@me.com", reply_email.from
     assert_equal "Das ist wunderschön", reply_email.stripped_body
